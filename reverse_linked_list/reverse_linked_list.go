8f8373ab1672ae4086f2540157447541152e5f39
gffone <94467554+gffone@users.noreply.github.com>
2024-11-04 03:29:25 +0300

    ?

---
+++ reverse_linked_list/reverse_linked_list.go
@@ -0,0 +1,64 @@
+package main
+
+type LinkedList struct {
+	head *Node
+	tail *Node
+	size int
+}
+
+type Node struct {
+	val  int
+	next *Node
+}
+
+func NewLL() LinkedList {
+	dummy := &Node{}
+	return LinkedList{dummy, dummy, 0}
+}
+
+func (l *LinkedList) Push(val int) {
+	newNode := Node{
+		val: val,
+	}
+	l.size++
+	l.tail.next = &newNode
+	l.tail = l.tail.next
+}
+
+func (l *LinkedList) Pop() int {
+
+	if l.head == l.tail {
+		return -1
+	}
+
+	l.size--
+	temp := l.head.next.val
+	l.head = l.head.next
+	return temp
+}
+
+func (l *LinkedList) Reverse() {
+
+	if l.size < 2 {
+		return
+	}
+
+	var prev *Node
+
+	l.tail = l.head.next
+	prev = l.tail
+	l.head = prev.next
+	prev.next = nil
+	for {
+		if l.head.next == nil {
+			dummy := &Node{}
+			l.head.next, prev, l.head = prev, l.head, dummy
+			l.head.next = prev
+			return
+		}
+		l.head.next, prev, l.head = prev, l.head, l.head.next
+	}
+}
+
+func main() {
+}
