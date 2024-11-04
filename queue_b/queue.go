e3cea1221b02b7be84965ad371336c6c3f964224
gffone <94467554+gffone@users.noreply.github.com>
2024-11-04 22:56:35 +0300

    ?

--- queue_b/queue.go
+++ queue_b/queue.go
@@ -1,67 +1,57 @@
-
 package main
 
 import (
 	"sync"
 )
 
-
 type Queue struct {
-	c *sync.Cond
+	c    *sync.Cond
+	head *Node
+	tail *Node
 	size int
-	firstNode *Node
-	lastNode *Node
 }
 
-type Node struct{
-	val int
+type Node struct {
+	val  int
 	next *Node
 }
 
-
 func NewQueue() *Queue {
-	cond:=sync.NewCond(&sync.Mutex{})
-	var f  Node
-	return &Queue{firstNode: &f, lastNode: &f, c:cond}
+	cond := sync.NewCond(&sync.Mutex{})
+	dummy := &Node{}
+	return &Queue{head: dummy, tail: dummy, c: cond}
 }
 
-
-func (q *Queue) Put(item int) {
+func (q *Queue) Push(item int) {
 	q.c.L.Lock()
 	defer q.c.L.Unlock()
 	q.size++
-	
-	newNode:= Node{}
-	q.lastNode.val = item
-	q.lastNode.next = &newNode
-	q.lastNode = q.lastNode.next
+
+	newNode := Node{}
+	q.tail.val = item
+	q.tail.next = &newNode
+	q.tail = q.tail.next
 	q.c.Signal()
 }
 
-
 // Если очередь пуста, блокируется до момента,
 // пока в очереди не появится элемент.
-func (q *Queue) Get() int {
+func (q *Queue) Pop() int {
 	q.c.L.Lock()
 	defer q.c.L.Unlock()
-	for q.size == 0{
+	for q.size == 0 {
 		q.c.Wait()
 	}
 	q.size--
-	v:=q.firstNode.val
-	q.firstNode = q.firstNode.next
-	return v
+	q.head = q.head.next
+	return q.head.val
 }
 
 func (q *Queue) Len() int {
 	q.c.L.Lock()
 	defer q.c.L.Unlock()
 
-	s:=q.size
+	s := q.size
 
 	return s
 }
-
-
-
-
